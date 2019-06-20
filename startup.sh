6f6a1b74de65993346d66f8f13c2e71358023a3b
jdob <jdobies@redhat.com>
2019-06-20 13:37:51 -0400

    Added retries to the database initialization due to timing issues

--- startup.sh
+++ startup.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-python manage.py migrate
+for i in {1..10}
+do
+    echo "Migrating Database..."
+    python manage.py migrate
+
+    if [ $? == "0" ]; then
+        echo "Migration Complete"
+        break
+    fi
+
+    sleep 3
+done
+
 python manage.py runserver 0.0.0.0:8000
